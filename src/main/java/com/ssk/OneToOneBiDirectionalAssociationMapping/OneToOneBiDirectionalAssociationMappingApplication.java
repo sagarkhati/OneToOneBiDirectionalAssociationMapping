672000d60664abc2d59fc85be1cca2d0c48c931a
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-07 23:32:56 +0530

    First Commit

---
+++ src/main/java/com/ssk/OneToOneBiDirectionalAssociationMapping/OneToOneBiDirectionalAssociationMappingApplication.java
@@ -0,0 +1,13 @@
+package com.ssk.OneToOneBiDirectionalAssociationMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneBiDirectionalAssociationMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneBiDirectionalAssociationMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssk/OneToOneBiDirectionalAssociationMapping/OneToOneBiDirectionalAssociationMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.OneToOneBiDirectionalAssociationMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneBiDirectionalAssociationMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
